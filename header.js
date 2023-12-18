3cc7283eac1307ee3e796dab852a580cbc21594d
crudy007 <65620918+crudy007@users.noreply.github.com>
2023-12-18 06:39:38 +0100

    Add files via upload

--- header.js
+++ header.js
@@ -2,17 +2,14 @@ class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class="tab-container">
-        <a href="index.html">
-    <div class="logo">
-        <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
-            <image href="Pictures/Sloth_baller.svg" width="50" height="50" />
-        </svg>
-    </div>
-        </a>
-
-
-
-            <a class="tab-button" id="homeTab">Home</a>
+            <a href="index.html" class="tab-button logo-container">
+                <div class="logo">
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                        <image href="Pictures/Sloth_baller.svg" width="50" height="50" />
+                    </svg>
+                </div>
+                Home
+            </a>
             <a class="tab-button" id="calculatorTab">Calculator</a>
             <a class="tab-button" id="contactTab">Contact</a>
             <input class="search-bar" type="text" placeholder="Search...">
@@ -26,7 +23,7 @@ class MyHeader extends HTMLElement {
         </div>`;
 
         // Add event listeners for tab clicks
-        const homeTab = this.querySelector("#homeTab");
+        const homeTab = this.querySelector(".logo-container");
         const calculatorTab = this.querySelector("#calculatorTab");
         const contactTab = this.querySelector("#contactTab");
 
